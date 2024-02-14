f4e1109053b57edacf0d6cbb48858c276ce6be03
Kyle-Yoon <113752652+Kyle-Yoon@users.noreply.github.com>
2024-02-14 17:05:27 +0900

    Add files via upload

---
+++ Two Pointer - 240214.py
@@ -0,0 +1,14 @@
+# 'nums'리스트 안의 두 가지 요소를 더해 'target' 숫자로 만들 수 있으면 True, 아니면 False
+
+#투 포인터로 풀이. O(nlogn)
+def twoSum(nums, target):
+    nums.sort()
+    l, r = 0, len(nums)-1
+    while l < r:
+        if nums[l] + nums[r] > target:
+            r -= 1
+        elif nums[l] + nums[r] < target:
+            l += 1
+        elif nums[l] + nums[r] == target:
+            return True
+    return False
