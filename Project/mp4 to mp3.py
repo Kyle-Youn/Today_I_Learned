e57a5124bb8b8105607afb5eec91781e78f60a00
Kyle-Yoon <113752652+Kyle-Yoon@users.noreply.github.com>
2024-06-29 23:46:52 +0900

    Update mp4 to mp3.py

--- Project/mp4 to mp3.py
+++ Project/mp4 to mp3.py
@@ -1,14 +1,15 @@
 '''
 pip install moviepy
+Have to download ffmpeg. https://doa-oh.tistory.com/170 참고하기!
 '''
 from moviepy.editor import *
 
 
 # MP4 원본파일 경로
-mp4_file = "C:/Users/Administrator/Downloads/1.mp4"  # 예: "/home/user/videos/video.mp4"
+mp4_file = "C:/Users/hjyou/Downloads/1.mp4"  # 예: "/home/user/videos/video.mp4"
 
 # MP3 파일이 저장될 경로와 파일 이름
-mp3_file = "C:/Users/Administrator/Downloads/1.mp3"  # 원하는 경로로 변경
+mp3_file = "C:/Users/hjyou/Downloads/1.mp3"  # 원하는 경로로 변경
 
 # 비디오 파일 불러오기
 video = VideoFileClip(mp4_file)
