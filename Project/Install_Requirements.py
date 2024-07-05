9d8b85b93789ef1aa1b96dc0dd0fe958fe0c990f
Kyle-Yoon <113752652+Kyle-Yoon@users.noreply.github.com>
2024-07-05 23:58:53 +0900

    Install_Requirements.py

---
+++ Project/Install_Requirements.py
@@ -0,0 +1,10 @@
+'''
+파이썬 깃 레포지토리 종속성 라이브러리 설치 명령어
+'''
+'''
+cmd를 띄운 다음 cd 명령어로 레포지토리 폴더로 이동한 다음 하단 명령어 실행
+pyenv-win, venv 반 필수!!!!!! 
+pyenv-win - venv - env 활성화 - 종속 라이브러리 설치 명령어 실행 순서 꼭 지키기
+'''
+
+pip install -r requirements.txt
