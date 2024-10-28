00043ce9ed3114a664c76257057df6b213cf3444
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2024-10-28 15:12:24 +0900

    Create PrivateAttribute.py

---
+++ Python_Grammer/PrivateAttribute.py
@@ -0,0 +1,15 @@
+class PayGildong:
+    def __init__(self):
+        self.day =25
+        self.__money = 100000
+        
+    def changeMoney(self, money):
+        self.__money = money
+
+    def getMoney(self):
+        return self.__money
+    
+
+gildong = PayGildong()
+# print(gildong.__money)    # 오류
+print(gildong.getMoney())
