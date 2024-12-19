73a1748fb2c68bd097d2b3727222d984b568a644
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2024-12-19 19:59:50 +0900

    Create uuid.py

---
+++ Project/uuid.py
@@ -0,0 +1,10 @@
+import uuid
+
+# 랜덤 UUID 생성
+ramdom_uuid = uuid.uuid4()
+print("Random UUID:", random_uuid)
+
+
+# 시간+MAC 주소 기반 UUID 생성
+time_uuid = uuid.uuid1()
+print("Time-based UUID:", time-uuid)
