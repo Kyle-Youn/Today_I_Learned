e7bc0f4907f8bfeacf48c568c19dfbae0684ab43
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2024-12-23 00:00:18 +0900

    Update basic_command.sh

--- Docker/basic_command.sh
+++ Docker/basic_command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 도커 이미지를 기반으로 새 컨테이너를 실행합니다.
-docker run -it --name my-container ubuntu /bin/bash
+docker run -it --name my-container ubuntu /bin/bash     # Ubuntu 기본 이미지를 사용, 컨테이너 실행 시 /bin/bash 명령 실행
 
 
 # 현재 실행 중인 컨테이너 목록을 확인
