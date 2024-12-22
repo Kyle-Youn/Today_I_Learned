9bbb1ce3656a95372933d2ddc63d68fc299fc9b2
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2024-12-22 23:58:30 +0900

    Create basic_command.sh

---
+++ Docker/basic_command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 도커 이미지를 기반으로 새 컨테이너를 실행합니다.
+docker run -it --name my-container ubuntu /bin/bash
+
+
+# 현재 실행 중인 컨테이너 목록을 확인
+docker ps
+# 모든 컨테이너(종료된 컨테이너 포함)를 확인
+docker ps -a
+
+
+# 로컬에 저장되어 있는 도커 이미지 목록을 확인
+docker images
