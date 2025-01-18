f4fcec093e108721fd7c5135cf5e039c1b03b19f
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2025-01-19 00:50:21 +0900

    Update daiquiri.py

--- Project/daiquiri.py
+++ Project/daiquiri.py
@@ -1,25 +1,3 @@
-# pip install daiquiri
-
-import daiquiri
-
-# Daiquiri 로깅 초기화
-daiquiri.setup()
-
-# 로거 가져오기
-logger = daiquiri.getLogger(__name__)
-
-# 로그 출력
-logger.info("This is an info message")
-logger.waring("This is a warning message")
-logger.error("This is a error message")
-
-
->>>>
-2024-01-01 12:00:00,000 INFO [module_name] This is an info message
-2024-01-01 12:00:00,001 WARNING [module_name] This is a warning message
-2024-01-01 12:00:00,002 ERROR [module_name] This is an error message
-
-
 import logging
 import daiquiri
 
