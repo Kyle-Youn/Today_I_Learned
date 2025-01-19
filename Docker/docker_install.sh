95f20ec1bd3cb5acd518272d2c61fc51d8202251
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2025-01-19 22:39:22 +0900

    Create docker_install.sh

---
+++ Docker/docker_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is 'Docker' download script to Ubuntu 24.04 server on RasberryPi 5
+
+sudo apt update
+sudo apt install apt-transport-https curl
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
