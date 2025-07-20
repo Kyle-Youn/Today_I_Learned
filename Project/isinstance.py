62283fd943597f599fc0a9cff19f55e8a3455912
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2025-07-21 00:27:04 +0900

    Update isinstance.py

--- Project/isinstance.py
+++ Project/isinstance.py
@@ -1,1 +1,13 @@
+x = 5    # int
+print(isinstance(x, int))    # True
+print(isinstance(x, float))    # False
 
+b= 5.0    # float
+print(a == b)    # True
+print(type(a) == type(b))    # Flase
+
+name = "파이썬"
+print(isinstance(name, str))    # True
+
+my_list = [1, 2, 3]
+print(isinstance(my_list, list))    # True
