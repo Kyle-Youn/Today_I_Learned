9bfb5b696583422ac935eda2cc87530a080aea27
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2025-12-30 23:07:18 +0900

    Update BloomFilter.py

--- Project/BloomFilter.py
+++ Project/BloomFilter.py
@@ -5,3 +5,11 @@ class BloomFilter:
     self.size = size
     self.num_hashes = num_hashes
     self.bit_array = [0] * size
+
+  def _hashed(self, item):
+    """여러 해시값 생성"""
+    result = []
+    for i in range(self.num_hashes):
+      h = hashlib.md5(f"{item}{i}".encode()).hexdigest()
+      result.append(int(h, 16) % self.size)
+    return result
