879a4e3b524773d9d9daaaf3c4f336933182af45
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2025-12-31 23:11:49 +0900

    Update BloomFilter.py

--- Project/BloomFilter.py
+++ Project/BloomFilter.py
@@ -13,3 +13,11 @@ class BloomFilter:
       h = hashlib.md5(f"{item}{i}".encode()).hexdigest()
       result.append(int(h, 16) % self.size)
     return result
+
+  def add(self, item):
+    """원소 추가"""
+    for pos in self._hashes(item):
+      self.bit_array[pos] = 1
+
+  def check(self, item):
+    return all(self.bit_array[pos] == 1 for pos in self._hashes(item))
