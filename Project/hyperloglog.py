463cfcd17caec7aaff5875afda64377210d1d767
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2026-01-10 23:52:00 +0900

    Update hyperloglog.py

--- Project/hyperloglog.py
+++ Project/hyperloglog.py
@@ -1,4 +1,7 @@
 import mmh3
 
 class HyperLogLog:
-  
+    def __init__(self, precision=14):
+        self.m = 1 << precision    # 2^precision buckets
+        self.buckets = [0] * self.m
+        self.alpha = self._get_alpha(self.m)
