da40d9ce1ce951fb1148fcc3453c5aab552aa40b
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2026-01-11 22:48:33 +0900

    Update hyperloglog.py

--- Project/hyperloglog.py
+++ Project/hyperloglog.py
@@ -5,3 +5,9 @@ class HyperLogLog:
         self.m = 1 << precision    # 2^precision buckets
         self.buckets = [0] * self.m
         self.alpha = self._get_alpha(self.m)
+
+    def _get_alpha(self, m):
+        if m >= 128: return 0.7213 / (1 + 1.079 / m)
+        elif m >= 64: return 0.709
+        elif m >= 32: return 0.697
+        return 0.5
