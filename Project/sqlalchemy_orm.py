ba4081eb70388827b13df2b902336a160b92e686
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2026-01-23 00:00:49 +0900

    Update sqlalchemy_orm.py

--- Project/sqlalchemy_orm.py
+++ Project/sqlalchemy_orm.py
@@ -6,3 +6,6 @@ engine = create_engine("postgresql://user:passowr@localhost/dbname")
 
 Session = sessionmaker(bind=engine)
 
+with Session() as session:
+  result = session.execute(text("SELECT * FROM users"))
+  session.commit()
