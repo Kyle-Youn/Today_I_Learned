f548122efbb1523b22267ad047b87522d55cbc31
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2026-01-22 23:59:41 +0900

    Create sqlalchemy_orm.py

---
+++ Project/sqlalchemy_orm.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+# 엔진 생성
+engine = create_engine("postgresql://user:passowr@localhost/dbname")
+
+Session = sessionmaker(bind=engine)
+
